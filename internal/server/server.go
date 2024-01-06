ba676aaeee87f6f07e98fc7d3481a1053905e66c
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2024-01-06 14:38:08 +0000

    added consume and produce endpoints

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+
 	api "github.com/abdulmajid18/log-service/api/v1"
 )
 
@@ -25,4 +27,21 @@ func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 		Config: config,
 	}
 	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
+	*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
 }
